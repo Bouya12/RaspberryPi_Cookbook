d567cf761c01e8ba3d5475d11f901c4903464c9f
Bouya <tsuredurebouya@gmail.com>
2019-06-06 23:42:22 +0900

    Create recipi8.6

---
+++ detect_motion.py
@@ -0,0 +1,15 @@
+from SimpleCV import*
+
+MIN_BLOB_SIZE = 1000
+
+c = Camera()
+
+old_image = c.getImage()
+
+while True:
+    new_image = c.getImage()
+    diff = new_image - old_image
+    blobs = diff.findBlobs(minsize=MIN_BLOB_SIZE)
+    if blobs:
+        print("Movement detected")
+    old_image = new_image
